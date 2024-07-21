0b4b2b654da9d1b30ffef1525f0ac6354b7a8c55
developerteams.in <developerteams.in@gmail.com>
2024-07-21 12:42:10 +0530

    d

--- Watch.php
+++ Watch.php
@@ -65,6 +65,34 @@
         <div class="w-40 h-40 bg-cover bg-center rounded-lg image-card animate" style="background-image: url('https://images.unsplash.com/photo-1721116858340-d955652c2fc8?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');"></div>
       </div>
     </div>
+    <div class="flex justify-center space-x-4 overflow-x-auto px-4">
+     <!-- Example Image Cards -->
+     <div id="section1" class="flex flex-col space-y-4 ">
+        <div class="w-40 h-56 bg-cover bg-center rounded-lg image-card animate" style="background-image: url('https://images.unsplash.com/photo-1573548500833-02035bc24fb6?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');"></div>
+        <div class="w-40 h-40 bg-cover bg-center rounded-lg image-card animate" style="background-image: url('https://plus.unsplash.com/premium_photo-1691854353831-bcc01370de49?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');"></div>
+        <div class="w-40 h-32 bg-cover bg-center rounded-lg image-card animate" style="background-image: url('https://plus.unsplash.com/premium_photo-1701192455496-a095e6e5f792?q=80&w=2400&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');"></div>
+      </div>
+      <div id="section2" class="flex flex-col space-y-4 mt-20">
+        <div class="w-40 h-40 bg-cover bg-center rounded-lg image-card animate" style="background-image: url('https://images.unsplash.com/photo-1710398866835-4c4ce11fe997?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');"></div>
+        <div class="w-40 h-56 bg-cover bg-center rounded-lg image-card animate" style="background-image: url('https://plus.unsplash.com/premium_photo-1673716791347-adb8336e72fb?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');"></div>
+        <div class="w-40 h-40 bg-cover bg-center rounded-lg image-card animate" style="background-image: url('https://images.unsplash.com/photo-1721417264655-2ccbf19152c6?q=80&w=2730&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');"></div>
+      </div>
+      <div id="section3" class="flex flex-col space-y-4 mt-32">
+        <div class="w-40 h-32 bg-cover bg-center rounded-lg image-card animate" style="background-image: url('https://images.unsplash.com/photo-1719937050792-a6a15d899281?q=80&w=2396&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');"></div>
+        <div class="w-40 h-40 bg-cover bg-center rounded-lg image-card animate" style="background-image: url('https://images.unsplash.com/photo-1721382487676-f63a96b68281?q=80&w=2499&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');"></div>
+        <div class="w-40 h-56 bg-cover bg-center rounded-lg image-card animate" style="background-image: url('https://images.unsplash.com/photo-1721297014055-c62b769944c2?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');"></div>
+      </div>
+      <div id="section4" class="flex flex-col space-y-4  mt-20">
+        <div class="w-40 h-32 bg-cover bg-center rounded-lg image-card animate" style="background-image: url('https://images.unsplash.com/photo-1721297013620-0f98628bbe98?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');"></div>
+        <div class="w-40 h-40 bg-cover bg-center rounded-lg image-card animate" style="background-image: url('https://images.unsplash.com/photo-1721153225946-4b8e0b2b1af6?q=80&w=2728&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');"></div>
+        <div class="w-40 h-56 bg-cover bg-center rounded-lg image-card animate" style="background-image: url('https://images.unsplash.com/photo-1721207106230-f0284889cc07?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');"></div>
+      </div>
+      <div id="section5" class="flex flex-col space-y-4 ">
+        <div class="w-40 h-40 bg-cover bg-center rounded-lg image-card animate" style="background-image: url('https://plus.unsplash.com/premium_photo-1700830647950-6bba9ca2a957?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');"></div>
+        <div class="w-40 h-56 bg-cover bg-center rounded-lg image-card animate" style="background-image: url('https://images.unsplash.com/photo-1721265250433-3e478e48f0f7?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');"></div>
+        <div class="w-40 h-40 bg-cover bg-center rounded-lg image-card animate" style="background-image: url('https://images.unsplash.com/photo-1721116858340-d955652c2fc8?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');"></div>
+      </div>
+    </div>
 
 
 <?php include "footer.php"; ?>
